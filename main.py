280388971f383f7ceff04c2bb88e1269c6c0c4a1
Ravi Pendurthi <ravipen28@gmail.com>
2025-03-08 13:29:49 +0100

    Application json response

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
 
 PORT = 8001
+msg = {'msg':'Hello!'}
+json_string = json.dumps(msg)
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
         self.end_headers()
-        self.wfile.write(b'Hello, World!')
+        self.wfile.write(json_string.encode(encoding='utf_8'))
 
 httpd = HTTPServer(('', PORT), SimpleHTTPRequestHandler)
 print('Starting server at port: http://localhost:' + str(PORT))
